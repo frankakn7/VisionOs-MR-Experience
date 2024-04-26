c6f8cf466d0610e411b5592dfa9e14c5b28b4292
Maxim Strzebkowski <maxim@Maxims-MacBook-Pro.local>
2024-04-26 17:29:43 +0200

    Initial xcode project setup

---
+++ MR-Experience/MR-Experience/MR_ExperienceApp.swift
@@ -0,0 +1,17 @@
+//
+//  MR_ExperienceApp.swift
+//  MR-Experience
+//
+//  Created by Maxim Strzebkowski on 26.04.24.
+//
+
+import SwiftUI
+
+@main
+struct MR_ExperienceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
